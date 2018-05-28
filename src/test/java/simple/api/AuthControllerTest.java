ae0bc42181cb7a6beb62b4a432ffa5d366ee8fb3
Paul Williams <paul@paulswilliams.me.uk>
2018-05-28 12:03:26 +0100

    Get implicitly available Pacts from broker

--- src/test/java/simple/api/AuthControllerTest.java
+++ src/test/java/simple/api/AuthControllerTest.java
@@ -2,7 +2,7 @@ package simple.api;
 
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRestPactRunner.class)
 @Provider("simple-auth-api")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@PactFolder("/Users/will/src/simple/web/target/pacts")
+@PactBroker(host = "localhost", port = "80")
 public class AuthControllerTest {
     @MockBean
     AuthService authService;
